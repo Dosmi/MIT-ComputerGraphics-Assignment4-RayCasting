5a969df874965092e529b017e5661955e5fd258f
sc17dd <dom@localhost.localdomain>
2020-08-28 22:14:45 +0100

    filled in Group Class to iterate through objects and intersect them with rays

--- Group.h
+++ Group.h
@@ -9,40 +9,70 @@
 
 using  namespace std;
 
-///TODO: 
+///3. @TODO:
 ///Implement Group
-///Add data structure to store a list of Object* 
+///Add data structure to store a list of Object*
 class Group:public Object3D
 {
 public:
 
-  Group(){
+  Group()
+  {}
 
-  }
-	
-  Group( int num_objects ){
-
-  }
-
-  ~Group(){
-   
+  Group( int num_objects )
+  {
+    m_objects.reserve(num_objects);
   }
 
-  virtual bool intersect( const Ray& r , Hit& h , float tmin ) {
-		
-   }
-	
-  void addObject( int index , Object3D* obj ){
+  ~Group()
+  {}
 
+  virtual bool intersect( const Ray& r , Hit& h , float tmin )
+  {
+    bool hit_object = false;
+    for (Object3D* object : m_objects)
+    {
+      // if successful hit, and previously hadn't hadn't hit anything ...
+      // ... then raise the flag such that some object has been hit:
+      if ( object->intersect(r, h, tmin) && !hit_object) hit_object = true;
+    }
+    return hit_object;
   }
 
-  int getGroupSize(){ 
-  
+  virtual float intersect_with_depth( const Ray& r , Hit& h , float tmin )
+  {
+    float min_depth = FLT_MAX;
+    // dbug dvar(m_objects.size()) eol;
+    for (Object3D* object : m_objects)
+    {
+      // dbug "callign " eol;
+      // if successful hit, and previously hadn't hadn't hit anything ...
+      // ... then raise the flag such that some object has been hit:
+      float depth = object->intersect_with_depth(r, h, tmin);
+      // dbug dvar(depth) eol;
+      if ( depth != tmin && depth < min_depth)
+      {
+        min_depth = depth;
+        // dbug dvar(depth) eol;
+      }
+    }
+    // dbug "returning " << dvar(min_depth) eol;
+    return min_depth;
+  }
+
+  void addObject( int index , Object3D* obj )
+  {
+    //dbug "adding object" eol;
+    m_objects.push_back(obj);
+  }
+
+  int getGroupSize()
+  {
+    return m_objects.size();
   }
 
  private:
-
+   std::vector<Object3D*> m_objects;
 };
 
 #endif
-	
