076ccb6e58524783fcc98782162430472016e847
sc17dd <dom@localhost.localdomain>
2020-08-30 22:34:50 +0100

    added normal debugging image output, also set-up the diffuse shadingg

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include <unordered_map>
 
 #include <typeinfo>
+#include "Light.h"
 
 using namespace std;
 
@@ -26,6 +27,12 @@ float clampedDepth ( float depthInput, float depthMin , float depthMax)
   if (depthInput < depthMin ) return 0.0f;
   if (depthInput > depthMax ) return 1.f;
   else return (depthInput-depthMin) / (depthMax-depthMin);
+}
+
+// 10. @TODO map a given normal vec3 to a vec 3 rgb color:
+Vector3f normal_to_rgb(const Vector3f& normal)
+{
+  return Vector3f(0.5) + normal * 0.5;
 }
 
 void saveToImage (int width, int height, const char* name,
@@ -43,6 +50,50 @@ void saveToImage (int width, int height, const char* name,
   }
   // image.SaveImage(argument_lookup.find("output")->second[0]);
   image.SaveImage(name);
+}
+
+float clamp(Vector3f vector1, Vector3f vector2)
+{
+  float dot = Vector3f::dot(vector1, vector2);
+  if ( dot > 0 ) return dot;
+  else return 0.f;
+}
+
+Vector3f ambientComponent(Vector3f ambient_light_color,
+                          Vector3f ambient_object_color)
+{
+  return ambient_light_color * ambient_object_color;
+}
+
+// @11
+Vector3f diffuseComponent(int num_lights,
+                          Light** scene_lights,
+                          // SceneParser& sp,
+                          const Vector3f& position,
+                          Vector3f& normal,
+                          const Vector3f& diffuse_color)
+{
+  Vector3f sum(0.f);
+  float m = 4.f;
+
+  for (int i = 0; i < num_lights; i++)
+  {
+    Vector3f direction_to_light(0.);
+    Vector3f light_color(0.);
+    scene_lights[i]->getIllumination(position, direction_to_light, light_color, m);
+    // sp.getLight(i)->getIllumination(position, direction_to_light, light_color, m);
+    float directionality = clamp(direction_to_light, normal);
+
+    sum += directionality * light_color * diffuse_color;
+  }
+
+  return sum;
+}
+
+// @15
+Vector3f specularComponent()
+{
+
 }
 
 
@@ -116,37 +167,87 @@ int main( int argc, char* argv[] )
 
   // for all pixels in the image plane, store the hit information:
   std::vector<Hit> pixel_hits;
-  std::vector<Vector3f> pixel_colors;
-  std::vector<Vector3f> pixel_depth;
+  std::vector<Vector3f> pixel_colors, pixel_normals, pixel_depth;
+
   pixel_hits.reserve(image_plane_width*image_plane_height);
   pixel_colors.reserve(image_plane_width*image_plane_height);
+  if (argument_lookup.find("normal") != argument_lookup.end() )
+    pixel_normals.reserve(image_plane_width*image_plane_height);
 
   int hit_counter = 0;
-
   const char* output_img_name = argument_lookup.find("output")->second[0];
 
   for (int x = 0; x <= image_plane_width; x++)
   {
     for (int y = 0; y <= image_plane_height; y++)
     {
-      //dbug x << " " << y eol;
+      int index = (image_plane_width+1)*x+y;
 
       PerspectiveCamera* scene_camera = (PerspectiveCamera*)sp.getCamera();
       // image plane is -1 to 1 from left to right, up to botton (2 units)
       Ray r = scene_camera->generateRay( Vector2f(-1 + (2/image_plane_width*x ),
                                                   -1 + (2/image_plane_height*y)) );
+      Material pixel_material(Vector3f(0.));
 
       pixel_hits.emplace_back(Hit()); // put empty pixel
 
+      Object3D* closest_object = nullptr;
+
       // Write the color at the intersection to that pixel ...
       if (sp.getGroup()->intersect(/* ray */ r,
-                                   /* hit */ pixel_hits[(image_plane_width+1)*x+y],
-                                   /* tmin*/ 0.01 ))
-        {
-          pixel_colors.emplace_back(Vector3f(1.));
-          hit_counter++;
+                                   /* hit */ pixel_hits[index],
+                                   /* tmin*/ 0.01,
+                                   /* closest_object */ closest_object  ))
+      {
+        if (argument_lookup.find("normal") != argument_lookup.end() )
+          // pixel_colors.emplace_back(normal_to_rgb(pixel_hits[index].getNormal()));
+          pixel_colors.emplace_back(pixel_hits[index].getNormal());
+        else
+        { // run fragment shader per pixel:
+          // pixel_colors.emplace_back(closest_object->getMaterial()->Shade(
+          // pixel_colors.emplace_back(pixel_hits[index].getMaterial()->Shade(
+          //   /* ray                */ r,
+          //   /* hit                */ pixel_hits[index],
+          //   /* direction_to_light */ Vector3f(0.),
+					// 	/* light_color        */ sp.getAmbientLight()
+          // ));
+
+          const Vector3f hit_position = r.getOrigin() + r.getDirection() * pixel_hits[index].getT();
+          Vector3f wh =  pixel_hits[index].getMaterial()->getDiffuseColor();
+          Vector3f normal = pixel_hits[index].getNormal();
+
+          Vector3f a = ambientComponent(
+          /* ambient_light_color  */ sp.getAmbientLight(),
+          /* ambient_object_color */ pixel_hits[index].getMaterial()->getAmbientColor());
+
+          Vector3f b = diffuseComponent(
+          /* num_lights    */ sp.getNumLights(),
+          /* scene_lights  */ sp.getLights(),
+          /* position      */ hit_position,
+          /* normal        */ normal,
+          /* diffuse_color */ wh  );
+
+          pixel_colors.push_back(a+b);
+
+          // pixel_colors.emplace_back(
+          //   ambientComponent(
+          //   /* ambient_light_color  */ sp.getAmbientLight(),
+          //   /* ambient_object_color */ pixel_hits[index].getMaterial()->getAmbientColor())
+          //   + diffuseComponent(
+          //   /* num_lights    */ sp.getNumLights(),
+          //   /* scene_lights  */ sp,//.getLights(),
+          //   /* position      */ hit_position,
+          //   /* normal        */ pixel_hits[index].getNormal(),
+          //   /* diffuse_color */ wh  ));
         }
-        else pixel_colors.emplace_back(Vector3f(0.));
+          // pixel_colors.emplace_back(Vector3f(0.));
+          // pixel_colors.emplace_back(pixel_material.Shade(
+          //   /* ray */ r, /* hit */  hit, /* direction_to_light */  ,
+          // 									const Vector3f& light_color )
+          // ));
+        hit_counter++;
+      }
+      else pixel_colors.emplace_back(Vector3f(0.));
 
       // if the user wants depth output as an image:
       if (argument_lookup.find("depth") != argument_lookup.end() )
